3c9c7510c4b8067ef913bfb7673b23f84a015ca6
Pablo Moreira Mora <pablo.moreiramora.com>
2014-01-13 23:36:44 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Obituario::Application.config.secret_key_base = 'c312f38d421f73f8dc19fa7078e4d448cd790efcfdb2b9800dfa1caca21893d948b3e5fa5039307ce58ec36e6f71587ad990c7ca6baf3eded749bcfcdf90c85e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Obituario::Application.config.session_store :cookie_store, key: '_obituario_session'
