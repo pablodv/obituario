e3a0be8f7a0ceab30e8bf4cf30a63f230e2819b9
Pablo Moreira Mora <pablo.moreiramora.com>
2014-01-15 20:38:26 -0300

    Enable assets

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(:default, Rails.env)
 module Obituario
   class Application < Rails::Application
     #config.assets.paths << "#{Rails}/vendor/assets/fonts"
-
+    config.assets.enabled = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
