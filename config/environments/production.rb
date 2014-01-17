5befd1e98580ec21238cb0923b2a0af050867441
Pablo Moreira Mora <pablo.moreiramora.com>
2014-01-17 18:58:35 -0300

    Config mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Obituario::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'obituario.herokuapp.com' }
 end
