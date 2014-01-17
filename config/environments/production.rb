c178fd189dfa87fef6c638626b48f047f01b0b96
Pablo Moreira Mora <pablo.moreiramora.com>
2014-01-17 19:16:16 -0300

    Fix action mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Obituario::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'obituario.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://obituario.herokuapp.com' }
 end
