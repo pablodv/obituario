568b8c160bee96bd730ff8a4f6f2e1653386a5f6
Pablo Moreira Mora <pablo.moreiramora.com>
2014-03-07 19:28:09 -0300

    Revert assets heroku configuration

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ module Obituario
     #config.assets.paths << "#{Rails}/vendor/assets/fonts"
     config.assets.enabled = true
 
-    # Disable Rails's static asset server (Apache or nginx will already do this).
-    config.serve_static_assets = true
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
