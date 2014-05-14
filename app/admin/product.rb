e9bdb29c24bc6f10cc707865e2c6a8d25c2bc965
Pablo Moreira Mora <pablo.moreiramora.com>
2014-05-13 22:42:55 -0300

    Add strong parameters on active admin

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -13,5 +13,5 @@ ActiveAdmin.register Product do
   #  permitted << :other if resource.something?
   #  permitted
   # end
-  
+  permit_params :name, :title, :summary, :description, :price, :kind
 end
